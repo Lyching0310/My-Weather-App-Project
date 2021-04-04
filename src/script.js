a2f2b55259250ce877653070db876e8e5075168e
Alyssa Reyes <alyssamarie_reyes@hotmail.com>
2021-04-04 21:30:51 +0100

    Added forecast API Test

--- src/script.js
+++ src/script.js
@@ -27,11 +27,19 @@ h3.innerHTML = `${currentHour}:${currentMinutes}`;
 
 //Forecast Element//
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class ="row"> `;
-  let days = ["Monday", "Tuesday", "Wednesday"];
+  let days = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Saturday",
+    "Sunday",
+  ];
   days.forEach(function (day) {
     forecastHTML =
       forecastHTML +
@@ -42,6 +50,15 @@ function displayForecast() {
             </div>`;
   });
   forecastElement.innerHTML = forecastHTML;
+}
+
+//Coordinates call for temp
+
+function getForecast(coordinates) {
+  let apiKey = "aff29a6b33c30edafe99104b632f71d7";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayForecast);
 }
 
 //Skeleton --->
@@ -73,6 +90,8 @@ function showTemp(response) {
   //weather type//
   let description = document.querySelector("#weatherType");
   description.innerHTML = response.data.weather[0].description;
+
+  getForecast(response.data.coords);
 }
 
 // City Default Search
