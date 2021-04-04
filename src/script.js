95f17bf7d3bb5b451d41be90df47a1dc5ee6fdbb
Alyssa Reyes <alyssamarie_reyes@hotmail.com>
2021-04-04 22:07:04 +0100

    forecast updated

--- src/script.js
+++ src/script.js
@@ -29,24 +29,23 @@ h3.innerHTML = `${currentHour}:${currentMinutes}`;
 
 function displayForecast(response) {
   console.log(response.data.daily);
+  //let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class ="row"> `;
-  let days = [
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Saturday",
-    "Sunday",
-  ];
-  days.forEach(function (day) {
+
+  forecast.forEach(function (forecastDay) {
     forecastHTML =
       forecastHTML +
       `<div class="col-sm-2" id="days">
-              ${day}
-              <i class="fas fa-cloud-sun"></i>
-              <p id="day-temp">12°</p>
+              ${forecastDay.dt}
+              <img 
+              src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
+              alt=""
+              width="42"
+              />
+
+              <p id="day-temp">${forecastDay.temp.day}°</p>
             </div>`;
   });
   forecastElement.innerHTML = forecastHTML;
@@ -55,6 +54,7 @@ function displayForecast(response) {
 //Coordinates call for temp
 
 function getForecast(coordinates) {
+  console.log(coordinates);
   let apiKey = "aff29a6b33c30edafe99104b632f71d7";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
 
@@ -91,7 +91,7 @@ function showTemp(response) {
   let description = document.querySelector("#weatherType");
   description.innerHTML = response.data.weather[0].description;
 
-  getForecast(response.data.coords);
+  getForecast(response.data.coord);
 }
 
 // City Default Search
